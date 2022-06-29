0b153748430a754f8d83e75aa746131fc9affe86
Nim Tayo <nimaester@gmail.com>
2022-06-29 14:09:18 -0700

    added chakra and nextjs to app

--- frontend-ecommerce/pages/index.js
+++ frontend-ecommerce/pages/index.js
@@ -1,10 +1,17 @@
-// import Head from "next/head";
-// import Image from "next/image";
+import Head from "next/head";
+import { background, Container } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Head>
+        <title>Ecommerce App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Container maxW='6xl' color='brand.700'>
+        <Text fontSize='5xl'>Main Page</Text>
+      </Container>
     </div>
   );
 }
