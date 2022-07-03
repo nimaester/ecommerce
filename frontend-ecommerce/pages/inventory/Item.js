4bfcb89fd98b8727b22c9ded9c15c0974716b2bd
Nim Tayo <nimaester@gmail.com>
2022-07-03 02:22:20 -0700

    adjusted image sizes

--- frontend-ecommerce/pages/inventory/Item.js
+++ frontend-ecommerce/pages/inventory/Item.js
@@ -1,30 +1,34 @@
 import React from "react";
 import { Flex, Text, Image, Box, Container } from "@chakra-ui/react";
 
-const Inventory = ({ item }) => {
+const Item = ({ item }) => {
   const { title, price } = item;
 
   return (
     <Flex
       flexDir='column'
       justifyContent='space-between'
-      m='2'
-      p='2'
+      m='1'
+      p='4'
       border='1px solid black'
-      maxW='xl'
+      alignItems='center'
     >
       <Image
+        objectFit='contain'
+        p='2'
         cursor='pointer'
-        w='40'
+        h='90%'
+        w='90%'
         src={item.image.data.attributes.formats.thumbnail.url}
         alt={item.title}
       />
-      <Box mt='5' cursor='pointer'>
+
+      <Box pt='4' mt='6' cursor='pointer'>
         <Text fontWeight='bold'>{title}</Text>
-        <Text>{price}</Text>
+        <Text>$ {price}</Text>
       </Box>
     </Flex>
   );
 };
 
-export default Inventory;
+export default Item;
