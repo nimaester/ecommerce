7a84ce4014b7efc2721b9f037e9d1577e7d7e5d7
Nim Tayo <nimaester@gmail.com>
2022-07-07 03:18:35 -0700

    fix the styling for hover effects

--- frontend-ecommerce/components/Item.js
+++ frontend-ecommerce/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
-import { Flex, Text, Image, Box, Container } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Flex, Text, Image, Box, PseudoBox } from "@chakra-ui/react";
 
 const Item = ({ item }) => {
   const { title, price } = item;
+  const [hover, setHover] = useState(false);
+
+  const mouseEnter = () => {
+    setHover(true);
+    console.log("enter");
+  };
+
+  const mouseOut = () => {
+    setHover(false);
+    console.log("out");
+  };
 
   return (
     <Flex
+      onMouseOver={mouseEnter}
+      onMouseOut={mouseOut}
       backgroundColor='white'
       borderRadius='10px'
       flexDir='column'
@@ -13,7 +26,18 @@ const Item = ({ item }) => {
       p='4'
       textAlign='right'
       justifyContent='space-between'
+      // _hover={{ backgroundColor: "red" }}
     >
+      <Box
+        display={hover ? "block" : "none"}
+        zIndex='50'
+        backgroundColor='red'
+        position='sticky'
+        top='0'
+        h='100%'
+        w='100%'
+      ></Box>
+
       <Box d='flex' margin='auto'>
         <Image
           objectFit='contain'
@@ -24,7 +48,18 @@ const Item = ({ item }) => {
         />
       </Box>
 
-      <Box pt='4' mt='6' cursor='pointer'>
+      <Box
+        pt='4'
+        mt='6'
+        cursor='pointer'
+        fontSize={{
+          sm: "0.8rem",
+          md: "1rem",
+          lg: "1rem",
+          xl: "1rem",
+          base: "0.8rem",
+        }}
+      >
         <Text fontWeight='bold'>{title}</Text>
         <Text>$ {price}</Text>
       </Box>
