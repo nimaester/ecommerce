e9fa31d01d49c64314351501581fe65ee3319dec
Nim Tayo <nimaester@gmail.com>
2022-07-11 22:03:58 -0700

    added a back button on item page

--- frontend-ecommerce/pages/item/[item].js
+++ frontend-ecommerce/pages/item/[item].js
@@ -1,18 +1,12 @@
-import {
-  Container,
-  Text,
-  Box,
-  Image,
-  Button,
-  Flex,
-  transition,
-} from "@chakra-ui/react";
+import { Container, Text, Box, Image, Button, Flex } from "@chakra-ui/react";
 import { useQuery } from "urql";
 import { GET_ITEM_INFO } from "../../lib/query";
 import { useRouter } from "next/router";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 const ItemDetail = () => {
   const { query } = useRouter();
+  const router = useRouter();
 
   // fectch item data from graphql api
   const [result] = useQuery({
@@ -27,39 +21,51 @@ const ItemDetail = () => {
   const { title, available, description, price, slug } = itemData;
 
   return (
-    <Container maxW='container.xl' display='flex' p='20'>
-      <Box flex='1.5' display='flex' alignItems='center'>
-        <Image
-          _hover={{ transform: "scale(1.1)", transition: "ease 0.1s" }}
-          src={itemData.image.data.attributes.formats.medium.url}
-          alt={slug}
-          maxW='90%'
-        />
-      </Box>
-      <Flex p='1rem' flex='1' flexDir='column' justifyContent='center'>
-        <Box>
-          <Text fontSize='4xl' pt='4' pb='4'>
-            {title}
-          </Text>
-          <Text>{description}</Text>
+    <Box>
+      <Container maxW='container.xl' display='flex' p='20'>
+        <Box flex='1.5' display='flex' alignItems='center'>
+          <Image
+            _hover={{ transform: "scale(1.1)", transition: "ease 0.1s" }}
+            src={itemData.image.data.attributes.formats.medium.url}
+            alt={slug}
+            maxW='90%'
+          />
         </Box>
-        <Box mt='5' display='flex' flexDir='row' justifyContent='space-between'>
-          <Button
-            _hover={{
-              backgroundColor: "brand.500",
-              color: "white",
-              border: `3px solid`,
-              borderColor: "brand.800",
-            }}
-            w='60%'
-            disabled={!available}
+        <Flex p='1rem' flex='1' flexDir='column' justifyContent='center'>
+          <Box>
+            <Text fontSize='4xl' pt='4' pb='4'>
+              {title}
+            </Text>
+            <Text>{description}</Text>
+          </Box>
+          <Box
+            mt='5'
+            display='flex'
+            flexDir='row'
+            justifyContent='space-between'
           >
-            {available ? "Add to cart" : "Not available"}
-          </Button>
-          <Text fontSize='2xl'>$ {price}</Text>
-        </Box>
-      </Flex>
-    </Container>
+            <Button
+              _hover={{
+                backgroundColor: "brand.500",
+                color: "white",
+                border: `3px solid`,
+                borderColor: "brand.800",
+              }}
+              w='60%'
+              disabled={!available}
+            >
+              {available ? "Add to cart" : "Not available"}
+            </Button>
+            <Text fontSize='2xl'>$ {price}</Text>
+          </Box>
+        </Flex>
+      </Container>
+      <Box pb='10' display='flex' justifyContent='center'>
+        <Button p='8' w='50%' cursor='pointer' onClick={() => router.back()}>
+          <MdKeyboardBackspace size='30' /> Back to previous page
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
