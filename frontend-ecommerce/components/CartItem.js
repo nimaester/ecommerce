8e7852eeaab7b2bf029991528e8360d43ac7e4ab
Nim Tayo <nimaester@gmail.com>
2022-07-19 21:49:50 -0700

    added remove button on cart

--- frontend-ecommerce/components/CartItem.js
+++ frontend-ecommerce/components/CartItem.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { useGlobalContext } from "../lib/storeContext";
 
 const CartItem = ({ cartItem }) => {
+  const { cart, setCart } = useGlobalContext();
+
+  const removeItem = (item) => {
+    const newCartItems = cart.filter((cartItem) => {
+      return cartItem.slug === item ? false : true;
+    });
+    setCart(newCartItems);
+  };
+
   return (
     <Box p='0rem 2rem'>
       <Flex p='2rem 0rem' justifyContent='space-between' alignItems='center'>
@@ -12,7 +22,16 @@ const CartItem = ({ cartItem }) => {
           alt={cartItem.slug}
         />
         <Text>{cartItem.name}</Text>
-        <Text>$ {cartItem.price}</Text>
+        <Box display='flex' flexDir='column' alignItems='center'>
+          <Text>$ {cartItem.price}</Text>
+          <Text
+            color='red'
+            _hover={{ cursor: "pointer" }}
+            onClick={() => removeItem(cartItem.slug)}
+          >
+            remove
+          </Text>
+        </Box>
       </Flex>
       <hr />
     </Box>
