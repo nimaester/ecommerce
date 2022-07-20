6103981e5d696d31da44b115ccf1021a9c8e9730
Nim Tayo <nimaester@gmail.com>
2022-07-19 20:48:35 -0700

    added continue shopping button on cart page

--- frontend-ecommerce/pages/cart.js
+++ frontend-ecommerce/pages/cart.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Container, Text } from "@chakra-ui/react";
+import { Container, Text, Box, Button } from "@chakra-ui/react";
+import { MdKeyboardBackspace } from "react-icons/md";
 import { useGlobalContext } from "../lib/storeContext";
+import { useRouter } from "next/router";
 import CartItem from "../components/CartItem.js";
 
 const Cart = () => {
   const { cart } = useGlobalContext();
+  const router = useRouter();
 
   return (
     <Container maxW='container.xl'>
@@ -22,6 +25,20 @@ const Cart = () => {
             }, 0)
           : "Your cart is empty!"}
       </Text>
+
+      <Box pb='10' display='flex' justifyContent='center'>
+        <Button
+          backgroundColor='transparent'
+          p='8'
+          w='90%'
+          cursor='pointer'
+          onClick={() => router.push("/")}
+          _hover={{ outline: "none", color: "button.primary" }}
+          _active={{ outline: "none", transform: "scale(1.05)" }}
+        >
+          <MdKeyboardBackspace size='30' /> Continue shopping
+        </Button>
+      </Box>
     </Container>
   );
 };
